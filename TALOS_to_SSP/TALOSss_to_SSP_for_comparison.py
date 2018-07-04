324c3486cf76e006ba11587716c8a68e4868f30c
Gaston Courtade <courtade.gaston@gmail.com>
2018-07-04 09:59:27 +0000

    TALOSss_to_SSP_for_comparison.py created online with Bitbucket

---
+++ TALOS_to_SSP/TALOSss_to_SSP_for_comparison.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import fileinput
+
+#Input file name
+inputFile = "predSS.tab"
+
+wordCount=0
+
+for line in fileinput.input(inputFile, inplace=1): 
+	sentence = line.split()
+	for word in sentence:
+		if word == 'FORMAT':
+			wordCount = wordCount + 1
+		if wordCount == 1 and len(sentence)==9:
+			ss = sentence[8]
+			res = sentence[0]
+			conf = float(sentence[7])
+			dict = {'L':0, 'H':1, 'E':-1}
+			if ss in dict:
+				ssp = conf*dict[ss]
+				line = res + '\t' + '%4.2f' % (ssp) + '\n'
+			else:
+				line=""
+		
+	print(line, end="")
+		
